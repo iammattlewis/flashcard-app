9fbe76c748341d912424970d7fe12415e53ed095
Matthew Lewis <matthewlewis@Matthews-MBP.lan>
2023-08-04 11:02:30 -0400

    attempt to fix editdeck not working

--- src/Layout/EditDeck.js
+++ src/Layout/EditDeck.js
@@ -70,7 +70,7 @@ function EditDeck() {
           </li>
         </ol>
       </nav>
-      <form>
+      <form onSubmit={submitHandler}>
         <h1 className="my-4 text-center">Edit Deck</h1>
         <div className="form-group">
           <label htmlFor="name">Name</label>
@@ -110,7 +110,6 @@ function EditDeck() {
         <button
           type="submit"
           className="btn btn-primary"
-          onSubmit={submitHandler}
         >
           Submit
         </button>
